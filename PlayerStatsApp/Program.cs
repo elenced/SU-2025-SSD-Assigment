b26684cf0463c041886565e3b59ec7fc740fe607
Eve Downs <evlenced@gmail.com>
2025-11-22 00:17:12 +0000

    Added a UI and Input

--- PlayerStatsApp/Program.cs
+++ PlayerStatsApp/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("First Github Push!");
+﻿using System;
+
+bool running = true; // creating a boolean to control the menu loop
+
+while (running)
+{
+    Console.WriteLine("──────‧₊˚ Player Statistics Manager ‧₊──────");
+    Console.WriteLine("1. Add New Player");
+    Console.WriteLine("2. View Players");
+    Console.WriteLine("3. Update Player Stats");
+    Console.WriteLine("4. Search Player by ID");
+    Console.WriteLine("5. Generate Report");
+    Console.WriteLine("6. Leave Application");
+    Console.Write("Select an option (1-6): ");
+
+    string choice = Console.ReadLine(); // number gets stored into the choice variable
+
+    switch (choice) // using the switch statment for the menu, avoiding the complexity of multiple if-else statements
+    {
+        case "1":
+            Console.WriteLine("Adding a new player...");
+            break;
+        case "2":
+            Console.WriteLine("Viewing players...");
+            break;
+        case "3":
+            Console.WriteLine("Updating player stats...");
+            break;
+        case "4":
+            Console.WriteLine("Searching player by ID...");
+            break;
+        case "5":
+
+            Console.WriteLine("Generating report...");
+            break;
+        case "6":
+            // Exit the application
+            running = false;
+            Console.WriteLine("Exiting the application!");
+            break;
+        default:
+            Console.WriteLine("Invalid input, try again.");
+            break;
+    }
+    Console.WriteLine(); 
+}
--- PlayerStatsApp/obj/Debug/net9.0/PlayerStatsApp.AssemblyInfo.cs
+++ PlayerStatsApp/obj/Debug/net9.0/PlayerStatsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayerStatsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deb5eb90b8fe0277773ed2fd87e40cdc5d3ac55b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5046b90d4049c3864a8c34d385fc23646f101555")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayerStatsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayerStatsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
